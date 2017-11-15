2903f18d7820beceb77f729232fd4683ffaec146
kian yamamoto <kian.yamamoto@gmail.com>
2017-11-14 22:47:51 -0600

    fixing

---
+++ calc/calc/Main.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Threading;
+
+namespace calc
+{
+    internal class Program
+    {
+        private static double? GetNumber(string prompt)
+        {
+            Console.Write(prompt);
+            string input = Console.ReadLine();
+            if (double.TryParse(input, out double number))
+            {
+                return number;
+            }
+            else
+            {
+                Console.WriteLine("\n input invalid ");
+                Thread.Sleep(800);
+                return null;
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("do you want to do a calculation ? [y/N]: ");
+                if (Console.ReadLine().ToLower().Equals("y"))
+                {
+                    Console.Write("enter mode\n1 = OpenCircuitVoltage\n: ");
+                    string mode = Console.ReadLine();
+                    switch (mode)
+                    {
+                        case "1":
+                            double? Isc = GetNumber("what system do you want to use \n 1 : Celsius, 2 : Fahrenheit\n: ");
+                            if (Isc == null) break;
+                            double? highTemp = GetNumber("enter T high: ");
+                            if (highTemp == null) break;
+                            double? lowTemp = GetNumber("enter T low: ");
+                            if (lowTemp == null) break;
+                            double high = 0;
+                            double low = 0;
+                            switch ((int)Isc)
+                            {
+                                case 1:
+                                    //converting C to Kelvin - with an extension method
+                                    //high = calc.Tcalc.Kalv((double)highTemp);
+                                    high = ((double)highTemp).ToKelvin();
+                                    //low = calc.Tcalc.Kalv((double)lowTemp);
+                                    low = ((double)lowTemp).ToKelvin();
+                                    break;
+
+                                case 2:
+                                    //converting F to C, then to Kelvin - with an extension method
+                                    //high = calc.Tcalc.Kalv(calc.Tcalc.Celc((double)highTemp));
+                                    high = (((double)highTemp).ToCelsius()).ToKelvin();
+                                    //low = calc.Tcalc.Kalv(calc.Tcalc.Celc((double)lowTemp));
+                                    low = (((double)lowTemp).ToCelsius()).ToKelvin();
+                                    break;
+
+                                default:
+                                    break;
+                            }
+
+                            //in order to call static methods without the "this" keyword, we just use the name of the static class
+                            Console.WriteLine(Tcalc.OpenCircuitVoltage(high, low));
+                            Thread.Sleep(5000);
+                            break;
+
+                        default:
+                            break;
+                    }
+
+                    /*Calc c = new Calc();
+                    Console.Write($"Multiplying {Number1} and {Number1}\n");
+                    double mult = c.MultiplyNumbers(Number1, Number1);
+                    Console.WriteLine(mult);*/
+                }
+                else break;
+                Console.Clear();
+            }
+
+            /* while (true)
+             {
+                 Console.Write("do you want to da a calculation ? [y/N]: ");
+                 if (Console.ReadLine().ToLower().Equals("y"))
+                 {
+                     Console.WriteLine("start");
+                     Console.Write("enter the first number: ");
+                     string input = Console.ReadLine();
+                     Console.Write("enter the second number: ");
+                     if (!double.TryParse(input, out double Number1))
+                     {
+                         Console.WriteLine("\n invalid input1 needs number");
+                         System.Threading.Thread.Sleep(800);
+                         goto end;
+                     }
+                     input = Console.ReadLine();
+                     if (!double.TryParse(input, out double Number2))
+                     {
+                         Console.WriteLine("\n invalid input2 needs number");
+                         System.Threading.Thread.Sleep(800);
+                         goto end;
+                     }
+                     //double mal = Number1 * Number2;
+                     double dev = Number1 / Number2;
+                     double paw = Math.Pow(Number1, Number2);
+                     double sub = Number1 - Number2;
+                     double add = Number1 + Number2;
+                     double rem = Number1 % Number2;
+                     Console.WriteLine(Number1);
+                     Console.WriteLine(Number2);
+                     //Console.WriteLine($"{Number1} * {Number2} = {Number1 * Number2}");
+                     Console.WriteLine(MultiplyNumbers(Number1, Number2));
+                     Console.WriteLine($"{Number1} / {Number2} = {Number1 / Number2}");
+                     Console.WriteLine($"{Number1} + {Number2} = {add}");
+                     Console.WriteLine($"{Number1} - {Number2} = {sub}");
+                     Console.WriteLine($"{Number1} ^ {Number2} = {Math.Pow(Number1, Number2)}");
+                     Console.WriteLine($"{Number2} % {Number2} = {Number1 % Number2}");
+                     Console.Write("press enter to continue...");
+                     Console.ReadLine();
+                 }
+                 else break;
+                 end: Console.Clear();       }*/
+        }
+    }
+}
---
+++ calc/calc/Tcalc.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace calc
+{
+    public static class Tcalc
+    {
+        public static double Celc(double Fin)
+        {
+            return (Fin - 32) / 1.8;
+        }
+
+        //trying this as an extension method might make it a little cleaner:
+        //https://weblogs.asp.net/scottgu/new-orcas-language-feature-extension-methods
+        //Scott Guthrie is an amazing programmer that ALWAYS wears a red shirt.
+        //that is why there is a red shirt on his blog!
+        //if you use the "this" keyword, you can call the static method a bit more efficiently...
+        public static double ToCelsius(this double fahrenheit)
+        {
+            return (fahrenheit - 32) / 1.8;
+        }
+
+        public static double Kalv(double Cin)
+        {
+            return Cin + 273.15;
+        }
+
+        //if you use the "this" keyword, you can call the static method a bit more efficiently...
+        public static double ToKelvin(this double celsius)
+        {
+            return celsius + 273.15;
+        }
+
+        public static double OpenCircuitVoltage(double high, double low)
+        {
+            return high - low / high;
+        }
+
+        //Specific Heat of Water, roughly 1BTU/pound mass degrees F
+        public static double CP = 1D;
+
+        //Temperature Difference - it is best to use the abs 
+        public static double ToDeltaTemp(double high, double low)
+        {
+            return Math.Abs(high - low);
+        }
+
+        /****
+         * http://www.ucsusa.org/sites/default/files/legacy/assets/documents/nuclear_power/20071204-ucs-brief-got-water.pdf
+         * The minimum flow rate of water needed in a once-through cooling system to remove the waste
+         * heat from a nuclear power reactor is given by the following equation:
+         * Q = m* CP * ΔT
+         * or
+         * m = Q / ( CP * ΔT) 
+         * where:
+         *      Q = waste heat load, BTU per hour
+         *      m = once - through cooling system flow rate, pounds mass per hour
+         *      CP = specific heat of water, ≈ 1 BTU/pound mass ºF
+         *      ΔT = temperature difference, ºF
+         ****/
+
+        // Q = m* CP * ΔT
+        public static double CalcMinFlowRateOneThrough(double onceThroughCoolingSystFlowRate, double highTemp, double lowTemp)
+        {
+            return onceThroughCoolingSystFlowRate * CP * ToDeltaTemp(highTemp, lowTemp);
+        }
+
+        // m = Q / ( CP * ΔT)
+        public static double CalcOnceThroughCoolingSystFlowRate(double wasteHeatLoad, double highTemp, double lowTemp)
+        {
+            return wasteHeatLoad /( CP * ToDeltaTemp(highTemp, lowTemp));
+        }
+
+    }
+}
